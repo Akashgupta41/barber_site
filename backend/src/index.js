cb9a50860ba926160091975b94874c2e37f7f230
Akash Gupta <153582655+Akashgupta41@users.noreply.github.com>
2025-01-08 17:25:59 +0530

    c

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,11 +31,12 @@ app.use("/api/review", reviewRoutes);
 const PORT = process.env.PORT;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+};
+
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
